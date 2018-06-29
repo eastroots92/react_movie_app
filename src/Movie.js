a452d9c5d6d8287964faf49750414c655386c951
Eastroots92 <eastroots92@gmail.com>
2018-06-29 11:48:08 +0900

    Studing React Componeents with JSX
    리액트 컴포넌트와 JSX의 구조 및 사용법을 이용해서 간단히 영화 이미지와 포스터를 올림

--- src/Movie.js
+++ src/Movie.js
@@ -3,8 +3,23 @@ import './Movie.css';
 
 class Movie extends Component {
   render() {
-    return <h1>Hello this is a Movie</h1>
+    return (
+      <div>
+        <MoviePoster />
+        <h1>Hello this is a Movie</h1>
+      </div>
+    )
   }
 }
+
+class MoviePoster extends Component {
+  render() {
+    return (
+      <img src="https://images-na.ssl-images-amazon.com/images/I/81nEtdN0f9L._SY606_.jpg" />
+    )
+  }
+}
+
+
 
 export default Movie;
