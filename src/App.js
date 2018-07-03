6778965ceae21dab4f38a4e08f2654c82c6aca8a
Eastroots92 <eastroots92@gmail.com>
2018-07-03 10:19:22 +0900

    Refactor "Movie" code
    Movie코드 중 하드 코딩이 되어있던 부분을 .map 함수를 이용하여 간략하게 정리

--- src/App.js
+++ src/App.js
@@ -2,28 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movieTitles = [
-  "Zootopia",
-  "Toy Story",
-  "UP",
-  "WALL-E"
-]
-
-const moviePosters = [
-  "https://images-na.ssl-images-amazon.com/images/I/81nEtdN0f9L._SY606_.jpg",
-  "https://i.pinimg.com/originals/59/82/e3/5982e3ae9bfb8a7f1f1491c75af69590.jpg",
-  "https://de1imrko8s7v6.cloudfront.net/movies/posters/up_1391712397.jpg",
-  "https://images-na.ssl-images-amazon.com/images/I/51RoZRgIHtL.jpg"
+const movies = [
+  {
+    title: "Zootopia",
+    poster: "https://images-na.ssl-images-amazon.com/images/I/81nEtdN0f9L._SY606_.jpg"
+  },
+  {
+    title: "Toy Story",
+    poster: "https://i.pinimg.com/originals/59/82/e3/5982e3ae9bfb8a7f1f1491c75af69590.jpg"
+  },
+  {
+    title: "UP",
+    poster: "https://de1imrko8s7v6.cloudfront.net/movies/posters/up_1391712397.jpg"
+  },
+  {
+    title: "WALL-E",
+    poster: "https://images-na.ssl-images-amazon.com/images/I/51RoZRgIHtL.jpg"
+  },
 ]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie title={movieTitles[0]} poster={moviePosters[0]} />
-        <Movie title={movieTitles[1]} poster={moviePosters[1]} />
-        <Movie title={movieTitles[2]} poster={moviePosters[2]} />
-        <Movie title={movieTitles[3]} poster={moviePosters[3]} />
+        {
+          movies.map((movie) => {
+            return <Movie title={movie.title} poster={movie.poster} />
+          })
+        }
       </div>
     );
   }
